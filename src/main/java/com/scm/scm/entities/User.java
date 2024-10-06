a93963431904ca5fc2e408936caa7491c9ecf673
Harshit <harshittiwari771@gmail.com>
2024-10-06 22:16:29 +0530

    pojo/entity classes

---
+++ src/main/java/com/scm/scm/entities/User.java
@@ -0,0 +1,81 @@
+package com.scm.scm.entities;
+import java.util.*;
+import java.util.stream.Collectors;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name="user")
+@Table(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements UserDetails{
+
+    @Id
+    private String userId;
+    @Column(name = "user_name",nullable = false)
+    private String name;
+    @Column(unique = true,nullable = false)
+    private String email;
+    @Getter(value = AccessLevel.NONE)
+    private String password;
+    @Column(length = 1000)
+    private String about;
+    @Column(length = 1000)
+    private String profilePic;
+    private String phoneNumber;
+
+    @Getter(value = AccessLevel.NONE)
+    private boolean enabled=true;
+    private boolean emailVerified=false;
+    private boolean phoneVerified=false;
+
+    @Enumerated(EnumType.STRING)
+    private Provider provider=Provider.SELF;
+    private String providerUserId;
+
+    private String emailToken;
+
+    @OneToMany(mappedBy = "user" ,cascade = CascadeType.ALL,fetch = FetchType.LAZY,orphanRemoval = true)
+    List<Contact> contacts=new ArrayList<>();
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roleList=new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<SimpleGrantedAuthority> roles = roleList.stream().map(role -> new SimpleGrantedAuthority(role))
+        .collect(Collectors.toList());
+            return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+   
+}
